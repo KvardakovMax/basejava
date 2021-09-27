6744c8db30d7d15783e9c3a325a1b51faba94ef9
Max Kvardakov <jimbomaxkvr@gmail.com>
2021-09-27 17:49:36 +0300

    Lesson 3 HW

--- src/com/topjava/webapp/storage/ArrayStorage.java
+++ src/com/topjava/webapp/storage/ArrayStorage.java
@@ -2,78 +2,27 @@ package com.topjava.webapp.storage;
 
 import com.topjava.webapp.model.Resume;
 
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
-    private Resume[] storage = new Resume[10_000];
-    private int size;
+public class ArrayStorage extends AbstractArrayStorage {
 
-    public int getIndex(String uuid) {
+    @Override
+    protected void insertElement(Resume r, int index) {
+        storage[size] = r;
+    }
+
+    @Override
+    protected void fillDeletedElement(int index) {
+        storage[index] = storage[size - 1];
+    }
+
+    protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
+            if (uuid.equals(storage[i].getUuid())) {
                 return i;
             }
         }
-        return size + 1;
-    }
-
-    public void save(Resume resume) {
-        if (size < storage.length) {
-            if (getIndex(resume.getUuid()) == size + 1) {
-                storage[size] = resume;
-                size++;
-            } else {
-                System.out.println("Such resume already exists");
-            }
-        } else {
-            System.out.println("Storage is full");
-        }
-    }
-
-    public void update(Resume resume) {
-        if (getIndex(resume.getUuid()) != size + 1) {
-            storage[getIndex(resume.getUuid())] = resume;
-        } else {
-            System.out.println("Such resume not found");
-        }
-    }
-
-    public Resume get(String uuid) {
-        if (getIndex(uuid) == size + 1) {
-            System.out.println("Resume not found");
-            return null;
-        }
-        return storage[getIndex(uuid)];
-    }
-
-    public void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
-        size = 0;
-    }
-
-    public void delete(String uuid) {
-        if (getIndex(uuid) == size + 1) {
-            System.out.println("Resume not found");
-        } else {
-            storage[getIndex(uuid)] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-        }
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
-    }
-
-    public int size() {
-        return size;
+        return -1;
     }
 }
