888282494d9791a46e60ee2c05cbc5ae08b55d54
Max Kvardakov <jimbomaxkvr@gmail.com>
2021-10-06 18:03:06 +0300

    HW4 Exceptions

--- test/com/topjava/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/topjava/webapp/storage/AbstractArrayStorageTest.java
@@ -73,7 +73,7 @@ public abstract class AbstractArrayStorageTest {
     @Test(expected = StorageException.class)
     public void saveOverflow() {
         try {
-            for (int i = 4; i <= AbstractArrayStorage.STORAGE_LIMIT + 1; i++) {
+            for (int i = 4; i <= AbstractArrayStorage.STORAGE_LIMIT; i++) {
                 storage.save(new Resume());
             }
         } catch (StorageException e) {
