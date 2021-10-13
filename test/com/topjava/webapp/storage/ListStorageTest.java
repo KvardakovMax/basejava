8c2dee46b1161d33d6790721dd6f4430cf5de1aa
Max Kvardakov <jimbomaxkvr@gmail.com>
2021-10-13 15:24:51 +0300

    HW5 part 1 and 2

--- test/com/topjava/webapp/storage/ListStorageTest.java
+++ test/com/topjava/webapp/storage/ListStorageTest.java
@@ -1,7 +1,5 @@
 package com.topjava.webapp.storage;
 
-import static org.junit.Assert.*;
-
 public class ListStorageTest extends AbstractArrayStorageTest{
 
     public ListStorageTest() {
--- test/com/topjava/webapp/storage/MapStorageTest.java
+++ test/com/topjava/webapp/storage/MapStorageTest.java
@@ -1,7 +1,5 @@
 package com.topjava.webapp.storage;
 
-import static org.junit.Assert.*;
-
 public class MapStorageTest extends AbstractArrayStorageTest{
 
     public MapStorageTest() {
